d0cdcc766b09df7295b96728b0a89fc4ba86eed6
keerthas <keerthas@INkeerthas.local>
2015-09-01 17:05:45 +0530

    Spec for division in interpreter class

--- src/com/thoughtworks/calculator/DivisionOperation.java
+++ src/com/thoughtworks/calculator/DivisionOperation.java
@@ -1,15 +1,23 @@
 package com.thoughtworks.calculator;
 
 public class DivisionOperation implements Operation{
-    private int operand2;
+    private int operand;
 
-    public DivisionOperation(int operand2) {
-        this.operand2 = operand2;
+    public DivisionOperation(int operand) {
+        this.operand = operand;
     }
 
     public double computeResult(double accumulator) {
-        if (operand2 != 0)
-            accumulator /= operand2;
+        if (operand != 0)
+            accumulator /= operand;
         return accumulator;
+    }
+
+    @Override
+    public boolean equals(Object that) {
+        if (!(that.getClass() == getClass()))
+            return false;
+        DivisionOperation thatDivisionOperation = (DivisionOperation) that;
+        return thatDivisionOperation.operand == operand;
     }
 }
