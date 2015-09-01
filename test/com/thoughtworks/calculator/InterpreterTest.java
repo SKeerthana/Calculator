9c1f2bd33ce76abaae0167db4626cfb5847af75f
keerthas <keerthas@INkeerthas.local>
2015-09-01 17:07:20 +0530

    Spec for division in interpreter class

--- test/com/thoughtworks/calculator/InterpreterTest.java
+++ test/com/thoughtworks/calculator/InterpreterTest.java
@@ -7,27 +7,34 @@ import static org.junit.Assert.assertEquals;
 public class InterpreterTest {
 
     @Test
-    public void shouldParseAddCommandWhichCallAdditionOperationConstructor(){
-        Interpreter interpreter=new Interpreter();
-        Operation operation=interpreter.parseToGetOperation("add 5");
+    public void shouldParseAddCommandWhichCallAdditionOperationConstructor() {
+        Interpreter interpreter = new Interpreter();
+        Operation operation = interpreter.parseToGetOperation("add 5");
 
-        assertEquals(new AdditionOperation(5),operation);
+        assertEquals(new AdditionOperation(5), operation);
     }
 
     @Test
-    public void shouldParseAddCommandWhichCallSubtractionOperationConstructor(){
-        Interpreter interpreter=new Interpreter();
-        Operation operation=interpreter.parseToGetOperation("subtract 5");
+    public void shouldParseAddCommandWhichCallSubtractionOperationConstructor() {
+        Interpreter interpreter = new Interpreter();
+        Operation operation = interpreter.parseToGetOperation("subtract 5");
 
-        assertEquals(new SubtractionOperation(5),operation);
+        assertEquals(new SubtractionOperation(5), operation);
     }
 
+    @Test
+    public void shouldParseAddCommandWhichCallMultiplicationOperationConstructor() {
+        Interpreter interpreter = new Interpreter();
+        Operation operation = interpreter.parseToGetOperation("multiply 5");
+
+        assertEquals(new MultiplicationOperation(5), operation);
+    }
 
     @Test
-    public void shouldParseAddCommandWhichCallMultiplicationOperationConstructor(){
-        Interpreter interpreter=new Interpreter();
-        Operation operation=interpreter.parseToGetOperation("multiply 5");
+    public void shouldParseAddCommandWhichCallDivisionOperationConstructor() {
+        Interpreter interpreter = new Interpreter();
+        Operation operation = interpreter.parseToGetOperation("divide 5");
 
-        assertEquals(new MultiplicationOperation(5),operation);
+        assertEquals(new DivisionOperation(5), operation);
     }
 }
