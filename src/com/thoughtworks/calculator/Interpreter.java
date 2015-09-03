1a23a0299b0376f448f82c3a0f78d08621582432
keerthas <keerthas@INkeerthas.local>
2015-09-03 06:23:48 +0530

    hooks testing

--- src/com/thoughtworks/calculator/Interpreter.java
+++ src/com/thoughtworks/calculator/Interpreter.java
@@ -5,7 +5,7 @@ public class Interpreter {
     public Operation parseToGetOperation(String command) {
 
         String[] splitCommand = command.split(" ");
-        if(splitCommand.length > 0) {
+        if(splitCommand.length == 2) {
             switch (splitCommand[0]) {
                 case "add":
                     return new AdditionOperation(Integer.parseInt(splitCommand[1]));
@@ -15,12 +15,12 @@ public class Interpreter {
                     return new MultiplicationOperation(Integer.parseInt(splitCommand[1]));
                 case "divide":
                     return new DivisionOperation(Integer.parseInt(splitCommand[1]));
-                case "cancel":
-                    return new CancelOperation();
-                default:
-                    return new InvalidOperation();
             }
         }
+
+        if (splitCommand.length ==1 && splitCommand[1].equals("cancel"))
+            return new CancelOperation();
+
         return new InvalidOperation();
     }
 }
